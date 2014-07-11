9ebab9834254e7fec85f04cdba933458033cc0be
Mark Raynsford <code@io7m.com>
2014-07-11 21:28:02 +0000

    Add file filter

--- src/main/java/com/io7m/sigiltron/SigiltronMainWindow.java
+++ src/main/java/com/io7m/sigiltron/SigiltronMainWindow.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -39,6 +39,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileFilter;
 
 import net.java.dev.designgridlayout.DesignGridLayout;
 
@@ -162,6 +163,19 @@ import com.io7m.jnull.Nullable;
 
           final JFileChooser dialog = new JFileChooser();
           dialog.setMultiSelectionEnabled(false);
+          dialog.setFileFilter(new FileFilter() {
+            @Override public String getDescription()
+            {
+              return "SVG files (*.svg)";
+            }
+
+            @Override public boolean accept(
+              @Nullable final File f)
+            {
+              final File fn = NullCheck.notNull(f, "File");
+              return fn.isDirectory() || fn.getName().endsWith(".svg");
+            }
+          });
 
           final int r = dialog.showSaveDialog(c);
           if (r == JFileChooser.APPROVE_OPTION) {
