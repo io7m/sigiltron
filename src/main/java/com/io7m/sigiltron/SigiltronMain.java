cfe44072c49b51a91036e74fc2114fe41519b97d
Mark Raynsford <code@io7m.com>
2014-07-12 12:54:55 +0000

    Remove comment. Update screenshot.

--- src/main/java/com/io7m/sigiltron/SigiltronMain.java
+++ src/main/java/com/io7m/sigiltron/SigiltronMain.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -56,7 +56,6 @@ public final class SigiltronMain
       Log.newLog(LogPolicyAllOn.newPolicy(LogLevel.LOG_DEBUG), "sigiltron");
 
     final SigiltronMainWindow win = new SigiltronMainWindow(log);
-    // win.setPreferredSize(new Dimension(640, 480));
     win.setTitle("Sigiltron");
     win.pack();
     win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
